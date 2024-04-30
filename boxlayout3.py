3af1e210b413fe2859373f1c4e86b1ef93a217d8
Letícia Aquino <131019654+aquinolet@users.noreply.github.com>
2024-04-30 15:22:38 -0300

    Update boxlayout3.py

--- boxlayout3.py
+++ boxlayout3.py
@@ -7,11 +7,11 @@ class MyApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical', spacing=10)
 
-        botao1 = Button(text='Botão 1', background_color=(0.2,0.7,0.3,1), font_size=20)
+        botao1 = Button(text='Botão 1', background_color=(0.2,0.7,0.3,1), font_size=20) 
         layout.add_widget(botao1)
-        botao2 = Button(text='Clique aqui!', haling='center')
+        botao2 = Button(text='Clique aqui!', halign='center')
         layout.add_widget(botao2)
-        botao3 = Button(text='Clique para continuar', bavkground_color=(0.9,0.5,0.1,1), font_size=30)
+        botao3 = Button(text='Clique para continuar', background_color=(0.9,0.5,0.1,1), font_size=30)
         layout.add_widget(botao3)
 
         def acao_botao(instance):
